ec0f125770dee3d1c2018cab47f5825d993094f3
Casey Kennington <bakuzen@gmail.com>
2018-11-02 10:06:52 -0600

    took out everyting related to nodes

--- suffixtree.py
+++ suffixtree.py
@@ -8,13 +8,12 @@ from segment import RootSegment, BaseSegment, InternalSegment, LeafSegment
 from sequence import Text
 from constants import Constants
 
-from sklearn import linear_model
 import numpy as np
 import random
 
 class SuffixTree:
     
-    def __init__(self, spec=(linear_model.LogisticRegression,{'penalty':'l2'})):
+    def __init__(self):
         self.text = Text()
         self.root = RootSegment(self.text)
         self.base = BaseSegment(self.root)
@@ -29,8 +28,6 @@ class SuffixTree:
         self.num_nodes = 1;
         self.num_leaves = 0;
         self.disc = 0.1
-        self.wac = {}
-        self.classifier_spec=spec
         
     def get_root(self):
         return self.root
@@ -119,81 +116,6 @@ class SuffixTree:
         for child in c.children:
             self.update_counts(c.children[child])
             
-    def train_node(self, parent_targets, parent, node):
-        if node is None: return
-        parent_target = self.ground_node(parent_targets, parent, node, 0) # make sure to the copy the list so it doesn't pass by ref
-        if parent_target is not None:
-            parent_targets.append(parent_target)
-        
-        if parent is not None:
-            offset=1
-            while offset < parent.span():
-                parent_target = self.ground_node(parent_targets, parent, node, offset)
-                if parent_target is not None: parent_targets.append(parent_target)
-                offset+=1
-            
-        if node.num_children() == 0: return
-        for child in node.children:
-            self.train_node(list(parent_targets), node, node.children[child])
-    
-    def train_nodes(self):
-        for child in self.get_root().children:
-            self.train_node(list(), None, self.get_root().children[child])
-            
-    def get_train_data_for_word(self, word):
-        todrop = ['word', 'inc', 'episode_id', 'id']
-        positive_train,negative_train = self.train_data
-        pos_word_frame = positive_train[positive_train.word == word]
-        pos_word_frame = np.array(pos_word_frame.drop(todrop, 1))
-        neg_word_frame = negative_train[negative_train.word == word]
-        neg_word_frame = np.array(neg_word_frame.drop(todrop, 1))
-        neg_word_frame = random.sample(list(neg_word_frame), len(pos_word_frame))
-        return np.array(pos_word_frame),np.array(neg_word_frame)
-        
-    
-    def ground_node(self, parent_targets, parent, child, offset): 
-        
-        
-        if offset > 0:
-            c_word = self.text.get_word_from_index(self.at(parent.get_left())+offset)
-        else:
-            c_word = self.text.get_word_from_index(self.text.at(child.get_left()))
-            
-        pos_word_frame,neg_word_frame = self.get_train_data_for_word(c_word)
-        
-        if len(pos_word_frame) == 0: return None
-        
-        X = np.concatenate([pos_word_frame,  neg_word_frame])
-        
-        if parent is None:
-            target = c_word
-        else:
-            p_word = self.text.get_word_from_index(self.text.at(parent.get_left()))
-            target = u'{}-{}'.format(p_word, c_word)
-            # need all parents
-            
-            if len(parent_targets) > 0:
-                X_pt = np.copy(X)
-                for parent_target in parent_targets:
-                    prediction = self.wac[parent_target].predict_proba(X_pt)[:,1]
-                    X_pt = np.concatenate([prediction.reshape(-1,1), X],axis=1) # adding in the parent data
-                X = X_pt
-            
-        
-        classifier, classf_params = self.classifier_spec
-        y = np.array([1] * len(pos_word_frame) + [0] * len(neg_word_frame))
-        
-        
-        this_classf = classifier(**classf_params)
-        this_classf.fit(X,y)
-        self.wac[target] = this_classf
-        
-        return target
-    
-    
-    def set_grounded_data(self, train_data):
-        self.train_data = train_data
-        
     def set_classifier_spec(self, spec):
         self.classifier_spec= spec
         
